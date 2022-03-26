89649bf182057ef48a5d4c23cfaeff6714a40dc8
jameshawkins26 <jrhawkins610@gmail.com>
2022-03-25 23:17:24 -0400

    Cleaned up and simplified multiples_of_three_or_five.java by combining both conditional statements into one.

--- multiples_of_three_or_five.java
+++ multiples_of_three_or_five.java
@@ -1,10 +1,14 @@
-// Class: multiples_of_three_or_five
-// This java class file computes the sum of multiples of 3 or 5 below 1000
-// Created by James Hawkins
+/* Class: multiples_of_three_or_five
+   This java class file computes the sum of multiples of 3 or 5 below 1000
+   Created by James Hawkins
+ */
 
 public class multiples_of_three_or_five {
-    // Method to get the sum of multiples of 3 or 5 below 1000
-    // Returns an int sum
+    /**
+     * The getSum() method calculates the sum of all multiples of 3 or 5 below 1000.
+     * It takes in no parameters.
+     * @return an int representing the sum of all multiples of 3 or 5 below 1000.
+     */
     public static int getSum() {
         // int baseNumber for the condition in the for loop
         int baseNumber = 1000;
@@ -15,14 +19,8 @@ public class multiples_of_three_or_five {
         // Increments currentNumber by 1 until it gets to the baseNumber 1000
         // currentNumber is an int that represents each digit below 1000
         for (int currentNumber = 0 ; currentNumber < baseNumber; currentNumber++) {
-            // Checks to see if the remainder of the number currentNumber is 0 when divided by 3
-            if (currentNumber % 3 == 0) {
-                // Add number currentNumber onto sum
-                sum += currentNumber;
-            }
-
-            // Checks to see if the remainder of the number currentNumber is 0 when divided by 5
-            else if (currentNumber % 5 == 0) {
+            // Checks to see if the remainder of the number currentNumber is 0 when divided by 3 or 5
+            if (currentNumber % 3 == 0 || currentNumber % 5 == 0) {
                 // Add number currentNumber onto sum
                 sum += currentNumber;
             }
@@ -34,7 +32,7 @@ public class multiples_of_three_or_five {
     // main method
     public static void main(String[] args) {
         // Print result of getSum() function
-        System.out.println(getSum());
+        System.out.println("Sum of all multiples of 3 or 5 below 1000: " + getSum());
     }
 
 }
